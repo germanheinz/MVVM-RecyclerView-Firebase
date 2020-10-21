e8eaa1d8966bf8b2e302309846870933d5eda500
germanheinz <heinz.german@gmail.com>
2020-10-21 02:57:52 +0200

    RecyclerView done!

--- app/src/main/java/com/example/mvvm_recyclerview_firebase_example/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_recyclerview_firebase_example/MainActivity.kt
@@ -16,12 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // TODO 1 - DECLARE RECYCLERVIEW, AND GIVE IT ORIENTATION, BY DEFAULT IS VERTICAL
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        //TODO 2 - CREATE ADAPTER - RECEIVE AN ARRAY AND ITERATE EACH ITEM TO PUT IT ON IN THE RECYCLERVIEW
-        recyclerView.adapter = MainAdapter(this)
+        //TODO 2 - CREATE ADAPTER - RECEIVE AN ARRAY AND ITERATE EACH ITEM TO PUT IT ON IN THE RECYCLERVIEW, INSTANCE
+        adapter = MainAdapter(this)
+
+        recyclerView.adapter = adapter
 
         val userList = mutableListOf<User>()
+        userList.add(User("https://static.wikia.nocookie.net/chespirito/images/9/93/El-chavo-del-8.jpg/revision/latest/top-crop/width/360/height/450?cb=20140212023312&path-prefix=es","User","testing MVVM - recycler view"))
+        userList.add(User("https://www.w3schools.com/howto/img_avatar.png","User","testing MVVM - recycler view"))
+        userList.add(User("https://www.w3schools.com/howto/img_avatar.png","User","testing MVVM - recycler view"))
         userList.add(User("https://www.w3schools.com/howto/img_avatar.png","User","testing MVVM - recycler view"))
 
         adapter.setListData(userList)
